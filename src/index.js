8ac7f5b5223dc888d4600b31680303f2652eb90d
Frankenfield <Kozyrov>
2018-02-20 20:34:09 -0600

    created basic video item and item list

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = "AIzaSyA_qUy1dbBR-YJ0ig0kHzwwrgc9qd2dl5I";
 
-const App = () => {
-    return (
-        <div>
-            <h3>Discipline</h3>
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+         YTSearch({key: API_KEY, term: "Spam Sandwiches"}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
             <div>
-                <SearchBar />
+                <h3>Discipline</h3>
+                <div>
+                    <SearchBar />
+                    <VideoList video={this.state.videos} />
+                </div>
             </div>
-        </div>
-    ); 
+        )
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
